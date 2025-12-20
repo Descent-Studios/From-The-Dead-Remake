e506fd847e25c8b12d777c5548fbb77491eedd51
FreepDryer <34213542+FreepDryer@users.noreply.github.com>
2025-12-20 15:50:11 -0600

    level decoration

--- scripts/levelGenerator/level_generator_main.gd
+++ scripts/levelGenerator/level_generator_main.gd
@@ -1,6 +1,8 @@
 extends Node3D
 class_name LevelGenerator
 
+
+@export var house_generate_tree : Node3D
 ## DO NOT MODIFY - THIS WILL BE GENERATED AT RUNTIME
 @export var house_generate_points : Array[Node3D]
 @export var colors_to_use : Array[GameManager.PackageColors]
@@ -22,17 +24,19 @@ func _ready():
 func find_houses(type : String):
 	var arr : Array[Node3D]
 	
-	for child in get_children():
+	for child in house_generate_tree.get_children():
 		if child.is_in_group(type):
 			arr.append(child)
 			
 	return arr
 
 func generate_level(colors : Array[GameManager.PackageColors]):
+	print_debug("Generating Level with Colors : ", colors)
+	print_debug("Using points : ", house_generate_points)
 	for point in house_generate_points:
 		if randi_range(0,20) >= 15:
 			var decoration : Node3D = decorations.pick_random().instantiate()
-			decoration.global_position = point.position
+			decoration.global_position = point.global_position
 			add_child(decoration)
 			continue
 		
